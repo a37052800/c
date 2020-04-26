743fc7ec4eccb485b9ea5b3988e42d1792387256
a37052800 <a37052800@gmail.com>
2020-04-26 21:15:51 +0800

    a006 coding

---
+++ 高中生程式/a006.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if((b*b-4*a*c)<0)
+    {
+        cout << "No real root";
+    }
+    else if((b*b-4*a*c)==0)
+    {
+        cout << "Two same roots x="<<(-b+pow(b*b-4*a*c,2))/(2*a);
+    }
+    else
+    {
+        cout << "";
+        
