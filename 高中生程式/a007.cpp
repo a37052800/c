fd486dac66bade43077c12b78ebc96cc09ea5987
a37052800 <a37052800@gmail.com>
2020-04-29 17:02:31 +0800

    a007

---
+++ 高中生程式/a007.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: a007.cpp
+    > Author: Wqr_
+    > Mail: [email protected] 
+    > Created Time: Wed Apr 29 12:46:35 2020
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
