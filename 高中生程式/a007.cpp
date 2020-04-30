e7275cdf47ae7d217a4569aa7ffa471393a4ecad
a37052800 <a37052800@gmail.com>
2020-04-30 14:06:45 +0800

    rea007.cpp

--- 高中生程式/a007.cpp
+++ 高中生程式/a007.cpp
@@ -1,15 +1,6 @@
-/*************************************************************************
-    > File Name: a007.cpp
-    > Author: Wqr_
-    > Mail: [email protected] 
-    > Created Time: Wed Apr 29 12:46:35 2020
- ************************************************************************/
-
 #include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<string>
-#include<vector>
-#include<algorithm>
 using namespace std;
-
+int main()
+{
+	return 0;
+}
