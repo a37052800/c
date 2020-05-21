e54ad57726baf64233a7b548938607b929cd6147
a37052800 <a37052800@gmail.com>
2020-05-21 17:25:48 +0800

    1023-1

--- contest/資訊之芽/NO1023-1.cpp
+++ contest/資訊之芽/NO1023-1.cpp
@@ -1,25 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int search(int &a[],int s,int u,int d,char f);
+int search(int &a[], int s, int u, int d, char f);
 int main()
 {
-	int n,q;
-	cin>>n>>q;
-	int a[n],l[q],r[q];
-	for(int i=0;i<n;i++)
-		cin>>a[i];
-	for(int i=0;i<q;i++)
-		cin>>l[i]>>r[i];
-	
+	int n, q;
+	cin >> n >> q;
+	int a[n], l[q], r[q];
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = 0; i < q; i++)
+		cin >> l[i] >> r[i];
 }
-int search(int &a[],int s,int u,int d,char f)
+int search(int &a[], int s, int u, int d, char f)
 {
-	int m=(u+d)/2;
-	if(f=='D')
+	int m = (u + d) / 2;
+	if (f == 'D')
 	{
-		if(u==d) return u;
-		if(a[m]<=s) search(a[],s,u,m,f);
-		if(a[m]>=s) search(a[],s,m,d,f);
-
+		if (u == d)
+			return u;
+		if (a[m] <= s)
+			search(a[], s, u, m, f);
+		if (a[m] >= s)
+			search(a[], s, m, d, f);
 	}
 }
