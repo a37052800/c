6b8e9efbc3d2bea19a66d2950199ab28c6d859bf
a37052800 <39836309+a37052800@users.noreply.github.com>
2020-06-17 18:36:46 +0800

    Create h185.cpp

---
+++ GreenJudge/h185.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int l=0;l<n;l++)
+	{
+		int ai,aj;
+		cin>>ai>>aj;
+		int a[ai][aj];
+		for(int i=0;i<ai;i++)
+		{
+			for(int j=0;j<aj;j++)
+			{
+				cin>>a[i][j];
+			}
+		}
+		for(int i=0;i<ai;i++)
+		{
+			
+		}
+	}
+}
