77ebad034b276531786c551e8af1f38081949eb6
a37052800 <a37052800@gmail.com>
2020-06-19 11:05:07 +0800

    h185

--- GreenJudge/h185.cpp
+++ GreenJudge/h185.cpp
@@ -1,36 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
 	int n;
-	cin>>n;
-	for(int l=0;l<n;l++)
+	cin >> n;
+	for (int l = 0; l < n; l++)
 	{
-		int ai,aj;
-		cin>>ai>>aj;
+		int ai, aj;
+		cin >> ai >> aj;
 		int a[ai][aj];
-		for(int i=0;i<ai;i++)
+		for (int i = 0; i < ai; i++)
 		{
-			for(int j=0;j<aj;j++)
+			for (int j = 0; j < aj; j++)
 			{
-				cin>>a[i][j];
+				cin >> a[i][j];
 			}
 		}
-		int x=a[ai][0];
-		for(int i=ai,j=0;i<ai&&j<aj;j++,i++)
+		for (int k = 0; k < aj - 1; k++)
 		{
-			if(x!=a[i][j])
+			for (int i = 0 + 1, j = k + 1; i < ai && j < aj; i++, j++)
 			{
-				cout<<"NO";
-				goto END;
-			}
-			if(i=ai)
-			{
-				i--;
-				x=a[i][0];
+				if (a[i][j] != a[i - 1][j - 1])
+				{
+					cout << "No" << endl;
+					goto ENDCC;
+				}
 			}
 		}
-		cout<<"YES"
-		END:;
+		for (int k = 0; k < ai - 1; k++)
+		{
+			for (int i = k + 1, j = 0 + 1; i < ai && j < aj; i++, j++)
+			{
+				if (a[i][j] != a[i - 1][j - 1])
+				{
+					cout << "No" << endl;
+					goto ENDCC;
+				}
+			}
+		}
+		cout << "Yes" << endl;
+	ENDCC:;
 	}
 }
