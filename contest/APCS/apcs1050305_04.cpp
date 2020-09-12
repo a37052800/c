52657e97a7c6fc2ed489d5154dec0f610bbea7d5
a37052800 <a37052800@gmail.com>
2020-09-12 21:14:57 +0800

    apcs1050305_04

--- contest/APCS/apcs1050305_04.cpp
+++ contest/APCS/apcs1050305_04.cpp
@@ -1,12 +1,14 @@
 #include <deque>
 #include <iostream>
 using namespace std;
+deque<int> tr[100001];
 int main()
 {
-    deque<int> tr[100001];
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
+        tr[i].clear();
+    for (int i = 0; i < n - 1; i++)
     {
         int tc, tf;
         cin >> tc >> tf;
