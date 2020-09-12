62e0a5ce7b41f32c6979348db92d1b41696f8aa2
a37052800 <a37052800@gmail.com>
2020-09-12 20:48:30 +0800

    apcs1050305_4

--- contest/APCS/apcs1050305_04.cpp
+++ contest/APCS/apcs1050305_04.cpp
@@ -1,41 +1,15 @@
+#include <deque>
 #include <iostream>
 using namespace std;
-class Homo
-{
-public:
-    Homo *f = NULL;
-    int c = 0;
-};
 int main()
 {
+    deque<int> tr[100001];
     int n;
     cin >> n;
-    Homo p[n - 1];
-    int temp[n][2];
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin >> temp[i][0] >> temp[i][1];
-    }
-    for (int i = 0; i < n - 1; i++)
-    {
-        p[i].c = temp[i][1];
-    }
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (temp[i][0] == p[j].c)
-            {
-                p[i].f = &p[j];
-            }
-        }
-    }
-    for (int i = 0; i < n - 1; i++)
-    {
-        cout << "name " << p[i].c;
-        if (p[i].f != NULL)
-        {
-            cout << " f " << p[i].f ->f ->c << endl;
-        }
+        int tc, tf;
+        cin >> tc >> tf;
+        tr[tf].push_back(tc);
     }
 }
--- test/樹狀結構.cpp
+++ test/樹狀結構.cpp
@@ -1,28 +1,51 @@
-#include<iostream>
-#include<list>
+#include <cstdio>
+#include <deque>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#define MAX 100001
 using namespace std;
-class Tree
-{
-	public:
-	int data;
-	Tree* parent;
-	list<Tree*> child;
-	void add_child(Tree *temp)
-	{
-		this->child.push_back(temp);
+deque<int> F[MAX];
+int v[MAX];
+void dfs(int x, int d) {
+	for (int i = 0; i<F[x].size(); i++) {//走訪每一個小孩與父母
+		if (v[F[x][i]] == 0) {
+			v[F[x][i]] = d + 1;
+			dfs(F[x][i], d + 1);
+		}
 	}
-};
-int main()
-{
-	Tree *p1,*p2,*p3;
-	p1=new Tree;
-	p1->data=0;
-	p2=new Tree;
-	p2->data=1;
-	p3=new Tree;
-	p3->data=2;
-	p1->add_child(p2);
-	p1->add_child(p3);
-	for(auto its=p1->child.begin();its!=p1->child.end();++its)
-		cout<<its.data<<" ";
+}
+
+int main() {
+	int a, b, node, n, max;
+	while (scanf("%d", &n) != EOF) {
+		for (int i = 0; i<n; i++) {
+			F[i].clear();
+		}
+		for (int i = 1; i<n; i++) {//建立無向圖
+			scanf("%d %d", &a, &b);
+			F[a].push_back(b);
+			F[b].push_back(a);
+		}
+		memset(v, 0, sizeof(v));
+		v[0] = 1;
+		dfs(0, 1);  //找出最遠的另一個端點
+		max = -1;
+		for (int i = 0; i < n; i++) {
+			if (v[i] > max) {
+				max = v[i];
+				node = i;
+			}
+		}
+		memset(v, 0, sizeof(v));
+		v[node] = 1;
+		dfs(node, 1);  //從最遠的端點，找尋最長路徑的長度，就是答案
+		for (int i = 0; i < n; i++) {
+			if (v[i] > max) {
+				max = v[i];
+			}
+		}
+		printf("%d\n", max-1);
+	}
 }
