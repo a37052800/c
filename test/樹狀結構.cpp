87165a2a5d5c18d884393ea4b7da4f0c8b17b694
a37052800 <a37052800@gmail.com>
2020-09-14 09:09:06 +0800

    tree

--- test/樹狀結構.cpp
+++ test/樹狀結構.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 using namespace std;
 class Tree
 {
-	public:
+public:
 	int data;
-	Tree* parent;
-	list<Tree*> child;
+	Tree *parent;
+	list<Tree *> child;
 	void add_child(Tree *temp)
 	{
 		this->child.push_back(temp);
@@ -14,15 +14,15 @@ class Tree
 };
 int main()
 {
-	Tree *p1,*p2,*p3;
-	p1=new Tree;
-	p1->data=0;
-	p2=new Tree;
-	p2->data=1;
-	p3=new Tree;
-	p3->data=2;
+	Tree *p1, *p2, *p3;
+	p1 = new Tree;
+	p1->data = 0;
+	p2 = new Tree;
+	p2->data = 1;
+	p3 = new Tree;
+	p3->data = 2;
 	p1->add_child(p2);
 	p1->add_child(p3);
-	for(auto its=p1->child.begin();its!=p1->child.end();++its)
-		cout<<its.data<<" ";
+	for (auto its = p1->child.begin(); its != p1->child.end(); ++its)
+		cout << &its->data << " ";
 }
