84405d70f26506068642eecbe809cb365f86d691
a37052800 <a37052800@gmail.com>
2020-09-17 23:36:38 +0800

    nood

---
+++ test/二元樹.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+class nood
+{
+    public:
+        nood *lift, *right;
+        int data;
+};
+int main()
+{
+    nood tree[7];
+    tree[0].data = 0;
+    tree[1].data = 1;
+    tree[2].data = 2;
+    tree[0].lift = &tree[1];
+    tree[0].right = &tree[2];
+    tree[1].lift = NULL;
+    tree[1].right = NULL;
+    tree[2].lift = NULL;
+    tree[2].right = NULL;
+}
