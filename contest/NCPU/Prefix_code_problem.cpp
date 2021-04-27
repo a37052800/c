57235c61db5ea6f637a9915f879d4470462d001e
a37052800 <a37052800@gmail.com>
2021-04-27 22:51:58 +0800

    0427

--- contest/NCPU/Prefix_code_problem.cpp
+++ contest/NCPU/Prefix_code_problem.cpp
@@ -1,40 +1,85 @@
 #include <deque>
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
 class nood
 {
 public:
-    char a;
-    nood *z;
-    nood *o;
+    char a = NULL;
+    nood *z = NULL;
+    nood *o = NULL;
 };
-deque<nood*> tr;
+deque<nood *> tr;
+void tp(nood *p)
+{
+    if (p->z != NULL)
+        tp(p->z);
+    cout << p->a;
+    if (p->o != NULL)
+        tp(p->o);
+};
 int main()
 {
     int n;
     cin >> n;
-    for (int i = 0; i < n;i++)
+    for (int i = 0; i < n; i++)
     {
         string df, px;
+        cin.clear();
+        cin.sync();
         getline(cin, df);
         getline(cin, px);
-        nood f;
-        tr.push_back(&f);
-        for (int i = 0; i < df.size;i++)
+        nood *f = new nood;
+        tr.push_back(f);
+        for (int j = 0; j < df.size(); j++)
         {
-            if(!((df[i]>'a')&&(df[i]<'z')))
+            if ((df[j] > 'a') && (df[j] < 'z'))
             {
-                nood new ne;
-                if(df[i]==0)
+                tr.back()->a = df[j];
+            }
+            else
+            {
+                nood *ne = new nood;
+                if (df[j] == 0)
                 {
-                    tr.back()->z = &ne;
+                    tr.back()->z = ne;
+                    tr.push_back(ne);
                 }
                 else
                 {
-                    tr.back()->o = &ne;
+                    for (int k = tr.size() - 2; k >= 0; k--)
+                    {
+                        if (tr[k]->o == NULL)
+                        {
+                            tr[k]->o = ne;
+                            tr.push_back(ne);
+                            break;
+                        }
+                    }
                 }
             }
         }
+        cout << tr[1]->a;
+        //tp(tr.front());
+        /*nood *stem = tr.front();
+        for (int i = 0; i < px.size(); i++)
+        {
+            if (stem->a != NULL)
+            {
+                cout << stem->a;
+                stem = tr.front();
+            }
+            else
+            {
+                if (px[i] == 0)
+                {
+                    stem = stem->z;
+                }
+                else
+                {
+                    stem = stem->o;
+                }
+            }
+        }*/
     }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,47 +1,15 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-int main(void)
-{
-	int m=0,n=0,x[100][100]={{0},{0}},i=0,j=0;
-	scanf ("%d%d",&m,&n);
-	for( i=0;i<m;i++)
-	{
-		for( j=0;j<n;j++)	scanf ("%d",&x[i][j]);
-	}
-	for( i=0;i<n;i++)
-	{
-		for( j=0;j<m;j++)
-		{
-			printf ("%d ",x[j][i]);
-		}
-		if(i!=(n-1))	puts("");
-	}
-}
-=======
 #include <iostream>
 using namespace std;
+class text
+{
+public:
+    char a = NULL;
+};
 int main()
 {
-    int x,c=0/*,i=1*/;
-    cin >> x;
-    /*while(i<x/i)
+    text *t = new text;
+    if (t->a != NULL)
     {
-        if(x%i==0)
-        {
-            c += i + (x / i);
-            i++;
-        }
-        else
-        {
-            i++;
-        }
-    }*/
-    for (int i = 1; i < x / i; i++)
-    {
-        if ((x % i) == 0)
-            c += i + (x / i);
+        cout << t->a;
     }
-    cout << c-x;
 }
->>>>>>> b2801d48e9c953e9f1e03c37861f86703437ed36
