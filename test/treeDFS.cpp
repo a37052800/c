8413ce7062ce9203794444373077e636a0b738da
a37052800 <a37052800@gmail.com>
2022-09-24 00:40:13 +0800

    coderunner op

--- test/treeDFS.cpp
+++ test/treeDFS.cpp
@@ -5,22 +5,20 @@ using namespace std;
 string dfs(string);
 int main()
 {
-    //vector<int> list;
+    // vector<int> list;
     string n = "01234";
     dfs(n);
 }
 string dfs(string n)
 {
-    string ans = "";
-    if (n.size() == 1)
-        return n;
-    else
+    string ans;
+    if (n.size() > 1)
     {
         for (int i = 0; i < n.size(); i++)
         {
             string t = n;
-            ans += dfs(t.erase(i, 1));
+            cout<<dfs(t.erase(i, 1))<<endl;
         }
     }
-    cout << ans;
+    return n;
 }
