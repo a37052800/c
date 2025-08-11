8c8ed5a1a4bc3544f1996be312ae5c0d98a5c624
Tim <39836309+a37052800@users.noreply.github.com>
2025-08-11 14:26:15 +0800

    0811

--- other/autossh.cpp
+++ other/autossh.cpp
@@ -8,20 +8,23 @@ int main()
     {
         WinExec("taskkill -im putty.exe -f", SW_HIDE);
         Sleep(1000);
-        WinExec("putty.exe -load sshrdp", SW_HIDE);
+        WinExec("putty.exe -load rdp", SW_HIDE);
         while (true)
         {
-            WinExec("PowerShell Test-NetConnection -ComputerName rdp.timsnet.uk -Port 4489 >result.txt", SW_HIDE);
+            WinExec("PowerShell Test-NetConnection -ComputerName simon901101.901101.xyz -Port 2161 >result.txt", SW_HIDE);
             Sleep(15000);
             std::ifstream fin;
             fin.open("result.txt");
+            // TcpTestSucceeded : True
             std::stringstream ss;
             ss << fin.rdbuf();
             fin.close();
-            if (ss.str().size() < 400)
+            if (ss.str().find("TcpTestSucceeded : True") != std::string::npos)
                 Sleep(30000);
             else
                 break;
         }
     }
 }
+
+// g++ -static -O3 -std=c++17 .\autossh.cpp -o .\autossh.exe
---
+++ other/avrng.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n = 2;
+    cout << n << '\n';
+    n = n << 4;
+    cout << n << '\n';
+    return 0;
+}
